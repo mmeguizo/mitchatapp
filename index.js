d94134450641f45f67e1694fde828e1b337d42bb
mmeguizo <mark.meguizo@gmail.com>
2024-04-13 00:10:44 +0800

    testers

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("*", (req, res) => {
-    // res.sendFile(path.join(__dirname, "/public/index.html"));
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+    // res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
 
 app.listen(port, () => {
