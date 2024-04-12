9c0c636139c47d4a5d4db9ab00a042c94abea87a
mmeguizo <mark.meguizo@gmail.com>
2024-04-13 00:08:46 +0800

    testers

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000; 
-
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    // res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
-  
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
