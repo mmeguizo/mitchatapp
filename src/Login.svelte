6be6bbe649d0607d81a8b1ad0f34767357b56475
mark <mark.meguizo@gmail.com>
2024-04-17 18:55:20 +0800

    add alert

--- src/Login.svelte
+++ src/Login.svelte
@@ -5,23 +5,51 @@ let username;
 let password;
 
 function logins() {
- login(username, password);
 
+
+  console.log(username, password)
+
+  if (username && password) {
+    if (username.trim().length >= 5 && username.trim().length <= 20) {
+      if (password.trim().length >= 5) {
+        login(username.trim(), password.trim());
+      } else {
+        alert('Password must be at least 5 characters long');
+      }
+    } else {
+      alert('Username must be at least 5 and not more than 20 characters long');
+    }
+  } else {
+    alert('Please fill in both fields');
+  }
 }
 
 function signups() {
- signup(username, password);
+
+  console.log(username, password)
+
+  if (username && password) {
+    if (username.trim().length >= 5 && username.trim().length <= 20) {
+      if (password.trim().length >= 5) {
+        signup(username.trim(), password.trim());
+      } else {
+        alert('Password must be at least 5 characters long');
+      }
+    } else {
+      alert('Username must be at least 5 and not more than 20 characters long');
+    }
+  } else {
+    alert('Please fill in both fields');
+  }
 }
-
-
 </script>
 
 <label for="username">Username</label>
-<input name="username" bind:value={username} minlength="3" maxlength="16" />
+<input name="username" bind:value={username} minlength="5" maxlength="20" />
 
 <label for="password">Password</label>
-<input name="password" bind:value={password} type="password" />
+<input name="password" bind:value={password} type="password" minlength="5" />
 
 <button class="login" on:click={logins}>Login</button>
-<button class="login"  on:click={signups}>Sign Up</button>
-  
+<button class="login" on:click={signups}>Sign Up</button>
+
