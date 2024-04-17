ce4440b29fee28e5bac0d929b6d5825cbe91b2f2
mark <mark.meguizo@gmail.com>
2024-04-17 09:57:33 +0800

    fix sender avatar

--- src/ChatMessage.svelte
+++ src/ChatMessage.svelte
@@ -4,17 +4,15 @@
 
   const messageClass = message.who === sender ? 'sent' : 'received';
 
-  const avatar = `https://i.pravatar.cc/150?u=${message.who}`;
-  // const avatar = `https://avatars.dicebear.com/api/initials/${message.who}.svg`;
+  const avatar = message.who === sender ? `https://i.pravatar.cc/150?u=${message.sender}` : `https://i.pravatar.cc/150?u=${message.who}`;
   
   const ts = new Date(message.when);
 </script>
-
 <div class={`message ${messageClass}`}>
   <img src={avatar} alt="avatar" />
   <div class="message-text">
     <p>{message.what}</p>
-
     <time>{ts.toLocaleTimeString()}</time>
   </div>
 </div>
+
